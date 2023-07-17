0633f9a59ce0d09942367cb7313b4f8ac442d3ac
Michael Bunch <Michael.Bunch@HHWAZ.ORG>
2023-07-17 11:37:51 -0700

    First commit

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
 # Michael Bunch
 # Day 22 of 100 days challenge PONG
-# Start 6/3/23
-# Finished
+# Start 7/3/23
+# Finished 7/7/23
 
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Paddle()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey("Up")
-screen.onkey("Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    #detect collision paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    #detect R paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #detect L paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,29 +1,18 @@
 from turtle import Turtle
 
-STARTING_POSITION = [(350, 0)]
-UP = 90
-DOWN = 270
-
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
-        self.create_paddle()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
-
-    def create_paddle(self):
-        for position in STARTING_POSITION:
-            self.shape("square")
-            self.shapesize(stretch_len=10)
-            self.penup()
-            self.color("white")
-            self.goto(position)
-
-
-
-    def up(self):
-        self.setheading(UP)
-
-
-    def down(self):
-        self.setheading(DOWN)
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
